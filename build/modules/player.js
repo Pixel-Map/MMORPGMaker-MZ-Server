14909d9aecd91ae6fec1bd961ab8da309d60c916
Mourad Kejji <mkejji@gmail.com>
2022-04-02 22:40:24 +0200

    Untracked build/ and package lockfiles

--- build/modules/player.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.initialize = function (mmoCore) {
-    var socket = mmoCore.socket;
-    socket.loadModules('player', true, mmoCore).then(function () {
-        console.log('[I] Sub modules of player loaded.');
-    });
-};
