252270aff10d337592d4a9df7e390072f3e5a8b3
Ken Erwin <ken@devopslibrary.com>
2022-07-01 20:49:55 -0400

    its beautiful

--- src/plugins/mmo_core.ts
+++ src/plugins/mmo_core.ts
@@ -1,42 +1,42 @@
-function MMO_Core() {
-    // Do Nothing
+class Core {
+    socket: any;
+    Parameters: any;
+    allowTouch: boolean;
+    private _onTouchStart: any;
+
+    constructor() {
+        this.Parameters = PluginManager.parameters('MMO_Core');
+        // @ts-ignore
+        this.socket = io(String(this.Parameters['Server Location']));
+        this.allowTouch = true;
+
+        // Wildcard for any disconnection from the server.
+        this.socket.on('disconnect', (reason) => {
+            document.dispatchEvent(new Event('mmorpg_core_lost_connection')); // Dispatch event for connection lost.
+            this.socket.close();
+            alert(reason);
+        });
+
+        // Clean up the menu
+        Window_MenuCommand.prototype.makeCommandList = function () {
+            this.addMainCommands();
+            this.addOriginalCommands();
+            this.addOptionsCommand();
+        };
+
+        TouchInput._onTouchStart = (event) => {
+            if (!this.allowTouch) return;
+            // @ts-ignore
+            MMO_Core._onTouchStart.call(this, event);
+        };
+
+        this._onTouchStart = TouchInput._onTouchStart;
+    }
+
+    sendMessage(message) {
+        if (message.length <= 0) return;
+        this.socket.emit('new_message', message);
+    }
 }
 
-MMO_Core.Parameters = PluginManager.parameters('MMO_Core');
-
-// @ts-ignore
-MMO_Core.socket = io(String(MMO_Core.Parameters['Server Location']));
-MMO_Core.allowTouch = true;
-
-// Wildcard for any disconnection from the server.
-MMO_Core.socket.on('disconnect', (reason) => {
-    document.dispatchEvent(new Event('mmorpg_core_lost_connection')); // Dispatch event for connection lost.
-    MMO_Core.socket.close();
-    alert(reason);
-});
-
-// Clean up the menu
-Window_MenuCommand.prototype.makeCommandList = function () {
-    this.addMainCommands();
-    this.addOriginalCommands();
-    this.addOptionsCommand();
-};
-// @ts-ignore
-MMO_Core._onTouchStart = TouchInput._onTouchStart;
-TouchInput._onTouchStart = function (event) {
-    if (!MMO_Core.allowTouch) return;
-    // @ts-ignore
-    MMO_Core._onTouchStart.call(this, event);
-};
-
-// ---------------------------------------
-// ---------- Exposed Functions
-// ---------------------------------------
-
-// @ts-ignore
-MMO_Core.sendMessage = function (message) {
-    if (message.length <= 0) return;
-    MMO_Core.socket.emit('new_message', message);
-};
-
-export default MMO_Core;
+export default new Core();
